04acdd42dc730b21fbc640d6cf997d21ba64a9ca
Jojo <jorys.kancel@gmail.com>
2025-03-30 18:36:17 +0200

    recup id assoc to don

--- js/association.js
+++ js/association.js
@@ -47,13 +47,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-function redirectToDon() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const id_assos = urlParams.get('id');
+document.addEventListener('DOMContentLoaded', () => {
+  const donationButton = document.getElementById('donation-button');
 
-  if (id_assos) {
-    window.location.href = `./paiement-don.html?id_assos=${id_assos}`;
-  } else {
-    alert("Erreur : ID de l'association introuvable.");
-  }
-}
+  donationButton.addEventListener('click', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id_assos = urlParams.get('id');
+
+    if (id_assos) {
+      window.location.href = `./paiement-don.html?id_assos=${id_assos}`;
+    } else {
+      alert("Erreur : ID de l'association introuvable.");
+    }
+  });
+});
